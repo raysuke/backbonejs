49fb6cde76c527b29f8d94a1a816389cfb3a551f
raysuke <kurosuke05185@yahoo.co.jp>
2018-02-28 16:48:19 +0900

    first commit

---
+++ js/app.js
@@ -0,0 +1,117 @@
+(function(){
+
+	// Modelのクラス定義
+	var Task = Backbone.Model.extend({
+		defaults: {
+			title: 'do something',
+			completed: false,
+		},
+		validate: function(attrs){
+			if(_.isEmpty(attrs.title)){
+				return 'title must not be empty';
+			}
+		},
+		initialize: function(){
+			this.on('invalid', function(model, error){
+				$('#error').html(error);
+			});
+		}
+	});
+	// Collectionのクラス定義
+	var Tasks = Backbone.Collection.extend({model: Task});
+
+	// Viewのクラス定義 (Model)
+	var TaskView = Backbone.View.extend({
+		tagName: 'li',
+		initialize: function(){
+			this.model.on('destroy', this.remove, this);
+			this.model.on('change', this.render, this);
+		},
+		events: {
+			'click .delete': 'destroy',
+			'click .toggle': 'toggle',
+		},
+		toggle: function(){
+			this.model.set('completed', !this.model.get('completed'));
+		},
+		destroy: function(){
+			if(confirm('are you sure?')){
+				this.model.destroy();
+			}
+		},
+		remove: function(){
+			this.$el.remove();
+		},
+		template: _.template($('#task-template').html()),
+		render: function(){
+			var template = this.template(this.model.toJSON());
+			this.$el.html(template);
+			return this;
+		},
+	});
+	// Viewのクラス定義 (Collection)
+	var TasksView = Backbone.View.extend({
+		tagName: 'ul',
+		initialize: function(){
+			this.collection.on('add', this.addNew, this);
+			this.collection.on('change', this.updateCount, this);
+			this.collection.on('destroy', this.updateCount, this);
+		},
+		addNew: function(task){
+			var taskView = new TaskView({model: task});
+			this.$el.append(taskView.render().el);
+			$('#title').val('').focus();
+			this.updateCount();
+		},
+		updateCount: function(){
+			var uncompletedTask = this.collection.filter(function(task){
+				return !task.get('completed');
+			});
+			$('#count').html(uncompletedTask.length);
+		},
+		render: function(){
+			this.collection.each(function(task){
+				var taskView = new TaskView({model: task});
+				this.$el.append(taskView.render().el);
+			}, this);
+			this.updateCount();
+			return this;
+		},
+	});
+
+	// テキストフォームの定義
+	var AddTaskView = Backbone.View.extend({
+		el: '#addTask',
+		events: {
+			'submit': 'submit',
+		},
+		submit: function(e){
+			e.preventDefault();
+			// var task = new Task({title: $('#title').val()});
+			// this.collection.add(task);
+			var task = new Task();
+			if(task.set({title: $('#title').val()},{validate: true})){
+				this.collection.add(task);
+				$('#error').empty();
+			}
+		}
+	});
+
+	var tasks = new Tasks([
+	{
+		title: 'task1',
+		completed: true,
+	},
+	{
+		title: 'task2',
+	},
+	{
+		title: 'task3',
+	},
+	]);
+
+	var tasksView = new TasksView({collection: tasks});
+	var addTaskView = new AddTaskView({collection: tasks});
+	$('#tasks').html(tasksView.render().el);
+
+})();
